a33c5e1252f840ea5ce2db3b5a323b73af99e3b4
Luis Theml <44423391+Theml@users.noreply.github.com>
2025-03-17 19:30:31 -0300

    Atualizar o LogisticaTrasnporteTest.java

--- LogisticaTrasnporteTest.java
+++ LogisticaTrasnporteTest.java
@@ -1,1 +1,13 @@
+class LogisticaTransporteTeste {
+ 
+ @Test
+ Void deveContratarTransporte() {
+ ILogistica logistica = LogisticaFactory.obterLogica("Transporte");
+assertEquals("Transporte contratado", logica.contratar());
 
+@Test
+ Void deveDemitirTransporte() {
+ ILogistica logistica = LogisticaFactory.obterLogica("Transporte");
+assertEquals("Transporte removido", logica.demitir());
+
+}
