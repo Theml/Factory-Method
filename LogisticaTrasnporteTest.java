b61b6433eaedbf8c76ecb0b169d60ce562ed274e
Luis Theml <44423391+Theml@users.noreply.github.com>
2025-03-18 19:29:25 -0300

    Update LogisticaTrasnporteTest.java

--- LogisticaTrasnporteTest.java
+++ LogisticaTrasnporteTest.java
@@ -3,11 +3,11 @@ class LogisticaTransporteTeste {
  @Test
  Void deveContratarTransporte() {
  ILogistica logistica = LogisticaFactory.obterLogica("Transporte");
-assertEquals("Transporte contratado", logica.contratar());
+assertEquals("Transporte contratado", logica.adicionar());
 
 @Test
  Void deveDemitirTransporte() {
  ILogistica logistica = LogisticaFactory.obterLogica("Transporte");
-assertEquals("Transporte removido", logica.demitir());
+assertEquals("Transporte removido", logica.remover());
 
 }
